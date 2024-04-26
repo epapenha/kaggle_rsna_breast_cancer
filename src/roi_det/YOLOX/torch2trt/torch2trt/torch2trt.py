405eaa0c47bded27b7e6e531032b703cec70c93b
Eric <epapenha@akaikaeru.com>
2024-04-26 17:43:26 -0400

    Debugging

--- src/roi_det/YOLOX/torch2trt/torch2trt/torch2trt.py
+++ src/roi_det/YOLOX/torch2trt/torch2trt/torch2trt.py
@@ -190,7 +190,10 @@ def broadcast_trt_tensors(network, trt_tensors, broadcast_ndim):
         broadcasted_trt_tensors = [None] * len(trt_tensors)
 
         for i, t in enumerate(trt_tensors):
-
+            print("DEBUG")
+            print(t)
+            print("*"*15)
+            print(t.shape)
             if len(t.shape) < broadcast_ndim:
                 # append 1 size dims to front
                 diff = broadcast_ndim - len(t.shape)
